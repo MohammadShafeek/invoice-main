526a913b54f9534b089bada30fe7e47669c50977
mohammad shafeek <ershafi770@gmail.com>
2025-12-06 00:43:09 +0530

    ggggg

--- invoicing-main/invoice/views.py
+++ invoicing-main/invoice/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+# You can add your view functions here.
+# For example:
+# from django.shortcuts import render
+
+# def home(request):
+#     return render(request, 'invoice/index.html')
+# # This is a simple view that renders the 'index.html' template from the 'invoice' app.
